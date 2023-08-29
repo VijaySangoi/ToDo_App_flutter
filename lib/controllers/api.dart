77a5e52b9543d21afb8330c38f46a2a13527d70e
vijay <vijaysangoi29@gmail.com>
2023-08-29 22:25:05 +0530

    get function

--- lib/controllers/api.dart
+++ lib/controllers/api.dart
@@ -2,8 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class api {
-  Future<http.Response> postrequest(url, data) async {
-    var response = await http.post(url, body: jsonEncode(data));
+  Future<http.Response> postrequest(url, header, data) async {
+    var response = await http.post(url, headers: header, body: jsonEncode(data));
+    return response;
+  }
+  Future<http.Response> getrequest(url, header, data) async {
+    var response = await http.get(url, headers: header);
     return response;
   }
 }
