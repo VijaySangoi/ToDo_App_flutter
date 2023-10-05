cbd5b4022357197cb3be09e3ef724c82b087e1da
vijay <vijaysangoi29@gmail.com>
2023-10-05 22:53:13 +0530

    homescreen rendering

--- lib/views/page/homeScreen.dart
+++ lib/views/page/homeScreen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:nothing_todo/vars/globals.dart';
 import '../../utilities/Helpers.dart';
@@ -12,23 +13,78 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeState extends State<HomeScreen> {
+  var interface = api();
+  var data;
+  var done;
+  var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    render();
+  }
+
+  render() async {
+    var url = "http://" + server + "/api/task";
+    var head = {
+      "content-type": "application/json",
+      "Authorization": "Bearer " + token
+    };
+    String response = await interface.getrequest(url, head);
+    if (response != "false") {
+      setState(() => {isLoaded = true});
+      data = json.decode(response);
+      data.forEach((var index, var item) => {
+            if (item["is_completed"] == "1")
+              {done.add(item), data.removeAt(index)}
+          });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Nothing-todo'),
-      ),
-      drawer: Helpers.sidebar(context),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Container(
-            child: Text('yet to build'),
-          );
-        },
-      )
-    );
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Nothing-todo'),
+        ),
+        drawer: Helpers.sidebar(context),
+        body: Visibility(
+          visible: isLoaded,
+          child: ListView.builder(
+            itemCount: data?.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.green)),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(data![index]['task'],
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.normal)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+        ));
   }
 }
